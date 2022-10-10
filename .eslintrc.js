97cfe24549f030eaf31f3555665a0e9c715fcaa4
HaydenLeDev <loicroth.pro@gmail.com>
2022-10-10 18:19:22 +0200

    html correction

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
     "overrides": [
     ],
     "parserOptions": {
-        "sourceType": "module"
+        "sourceType": "module",
+        "ecmaVersion": 8
     },
     "rules": {
     }
